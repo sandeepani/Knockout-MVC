0b6d18c47896a7f32efa36c0660866a6e8a651e2
sandeepani <h.sandeepani@gmail.com>
2018-08-15 12:16:25 +0530

    Add files via upload

---
+++ MvcApplication1/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        static readonly IRepository<Product> repository = new ProductRepository();
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllProducts()
+        {
+            return Json(repository.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddProduct(Product item)
+        {
+            item = repository.Add(item);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult EditProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (repository.Update(product))
+            {
+                return Json(repository.GetAll(), JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(null);
+        }
+
+        public JsonResult DeleteProduct(int id)
+        {
+
+            if (repository.Delete(id))
+            {
+                return Json(new { Status = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { Status = false }, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/SaleController.cs
@@ -0,0 +1,62 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class SaleController : Controller
+    {
+
+        static readonly IRepository<Sale> repository = new SaleRepository();
+        static readonly IRepository<Product> prodRepository = new ProductRepository();
+
+        public ActionResult Sales()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllSales()
+        {
+            return Json(repository.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddSale(Sale item)
+        {
+            item.CreatedDate = DateTime.Now;
+            item = repository.Add(item);
+            return Json(item, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult EditSale(int id, Sale Sale)
+        {
+            Sale.Id = id;
+            if (repository.Update(Sale))
+            {
+                return Json(repository.GetAll(), JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(null);
+        }
+
+        public JsonResult DeleteSale(int id)
+        {
+
+            if (repository.Delete(id))
+            {
+                return Json(new { Status = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { Status = false }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult GetAllProducts()
+        {
+            return Json(prodRepository.GetAll(), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
